a618a300040f0f8c858c9fbc248168021844dc46
alkhiny <dialloakh@gmail.com>
2019-10-25 21:56:53 +0200

    première version de blackjack

---
+++ carte/blackjack/src/blackjack/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package blackjack;
+
+import cartes.Carte;
+
+/**
+ *
+ * @author 21912873
+ */
+public class Main {
+    public static void main(String[] args)
+    {
+        Carte c5 = new Carte("blackJack", "pique");
+        System.out.println(c5.toString());
+        
+    }
+}
